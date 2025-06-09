3c8b7f45f438ccd13d1942fd137b503fa8559bb2
LinMihigo <linmihigo@gmail.com>
2025-06-09 22:35:14 +0200

    Task 0 and 1

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message to the STDOUT
+ * @param {String} message Message to display
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
