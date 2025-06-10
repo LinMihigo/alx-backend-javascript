15eff9dc15ddd86c1fb1d9c7c16e60e585b7894b
LinMihigo <linmihigo@gmail.com>
2025-06-10 22:54:38 +0200

    Task 2 to 8

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,13 @@
-const readline = require('readline');
+process.stdout.write('Welcome to ALX, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-
-process.stdout.write("Welcome to ALx, what is your name?\n");
-
-rl.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input}\n`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
